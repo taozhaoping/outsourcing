c55adcd01b64c8e27ef5fa9bbccbae954a619110
caiyingying <caiyingying234@gmail.com>
2014-07-13 21:49:14 +0800

    添加权限以及目录的action

---
+++ src/com/zh/base/action/MenuAction.java
@@ -0,0 +1,46 @@
+package com.zh.base.action;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.zh.base.model.MenuModel;
+import com.zh.base.model.bean.Menu;
+import com.zh.base.service.MenuService;
+import com.zh.core.base.action.Action;
+import com.zh.core.base.action.BaseAction;
+
+public class MenuAction extends BaseAction {
+
+	/**
+	 * 角色Action
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private static Logger LOGGER = LoggerFactory.getLogger(MenuAction.class); 
+	
+	private MenuModel menuModel = new MenuModel();
+	
+	@Autowired
+	private MenuService menuService;
+
+	@Override
+	public Object getModel() {
+		return menuModel;
+	}
+	
+	/**
+	 * 角色管理入口
+	 */
+	public String execute() {
+		LOGGER.debug("execute() ");
+		
+		Menu menu = this.menuModel.getMenu();
+		List<Menu> menuList = menuService.queryList(menu);
+		this.menuModel.setMenuList(menuList);
+		return Action.SUCCESS;
+	}
+
+}
